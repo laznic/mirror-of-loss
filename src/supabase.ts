f67edfe3766de749181f21eb1975571910c24072
Niklas L <niklas@demtrees.com>
2023-12-11 21:54:23 +0200

    Add Supabase client

---
+++ src/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    realtime: {
+      params: {
+        eventsPerSecond: 1,
+      },
+    },
+  }
+);
+
+export default supabase;
