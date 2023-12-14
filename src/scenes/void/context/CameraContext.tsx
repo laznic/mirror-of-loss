d428b6f27c53ba6c04c3eac157466d2fd260ec91
Niklas L <niklas@demtrees.com>
2023-12-14 18:23:31 +0200

    Add some contexts

---
+++ src/scenes/void/context/CameraContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+import { Vector3 } from "three";
+
+interface CameraContext {
+  camPos: Vector3;
+  lookAt: Vector3;
+  setCamPos: (position: Vector3) => void;
+  setLookAt: (position: Vector3) => void;
+}
+
+export const CameraContext = createContext<CameraContext>({
+  camPos: new Vector3(),
+  lookAt: new Vector3(),
+  setCamPos: () => {},
+  setLookAt: () => {},
+});
+
+interface CameraContextProviderProps {
+  children: React.ReactNode;
+}
+
+export default function CameraContextProvider({
+  children,
+}: CameraContextProviderProps) {
+  const [camPos, setCamPos] = useState(new Vector3(0, 0, 0));
+  const [lookAt, setLookAt] = useState(new Vector3(0, 0, 0));
+
+  return (
+    <CameraContext.Provider value={{ camPos, lookAt, setCamPos, setLookAt }}>
+      {children}
+    </CameraContext.Provider>
+  );
+}
+
+export function useCameraContext() {
+  return useContext(CameraContext);
+}
---
+++ src/scenes/void/context/MemoryGroupContext.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface MemoryGroupContext {
+  currentGroup: number | null;
+  setCurrentGroup: (id: number | null) => void;
+}
+
+export const MemoryGroupContext = createContext<MemoryGroupContext>({
+  currentGroup: null,
+  setCurrentGroup: () => {},
+});
+
+interface MemoryGroupContextProviderProps {
+  children: ReactNode;
+}
+
+export default function MemoryGroupContextProvider({
+  children,
+}: MemoryGroupContextProviderProps) {
+  const [currentGroup, setCurrentGroup] = useState<number | null>(null);
+
+  return (
+    <MemoryGroupContext.Provider value={{ currentGroup, setCurrentGroup }}>
+      {children}
+    </MemoryGroupContext.Provider>
+  );
+}
