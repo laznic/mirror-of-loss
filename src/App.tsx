86bccb9aacd3d9e4aee679fdc7d1d5dd691fa385
Niklas L <niklas@demtrees.com>
2023-12-16 21:41:50 +0200

    Move keyboard control back to higher level

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,16 @@ import { MirrorScene } from "./scenes/mirror";
 import VoidScene from "./scenes/void";
 import { useEffect } from "react";
 
+const keyboardMap = [
+  { name: "forward", keys: ["ArrowUp", "w", "W"] },
+  { name: "backward", keys: ["ArrowDown", "s", "S"] },
+  { name: "leftward", keys: ["ArrowLeft", "a", "A"] },
+  { name: "rightward", keys: ["ArrowRight", "d", "D"] },
+  { name: "jump", keys: ["Space"] },
+  { name: "run", keys: ["Shift"] },
+  { name: "crouch", keys: ["c", "C"] },
+];
+
 function App() {
   useEffect(() => {
     if (!localStorage.getItem("uuid")) {
@@ -21,17 +31,19 @@ function App() {
         style={{ width: "100%", height: "100%" }}
         onPointerDown={(e) => e.target.requestPointerLock()}
       >
-        <ambientLight color={"#fff"} />
+        <KeyboardControls map={keyboardMap}>
+          <ambientLight color={"#fff"} />
 
-        {/* <fog color="#000" attach="fog" near={50} far={700} /> */}
-        <directionalLight
-          intensity={5}
-          color={"#fff"}
-          position={[-3, 1.5, 3]}
-        />
+          {/* <fog color="#000" attach="fog" near={50} far={700} /> */}
+          <directionalLight
+            intensity={5}
+            color={"#fff"}
+            position={[-3, 1.5, 3]}
+          />
 
-        {/* <VoidScene /> */}
-        <MirrorScene />
+          {/* <VoidScene /> */}
+          <MirrorScene />
+        </KeyboardControls>
       </Canvas>
     </>
   );
--- src/scenes/mirror/index.tsx
+++ src/scenes/mirror/index.tsx
@@ -1,4 +1,4 @@
-import { KeyboardControls, Sphere } from "@react-three/drei";
+import { Sphere } from "@react-three/drei";
 import Brazier from "./components/Brazier";
 import MemoryBlobs from "./components/MemoryBlobs";
 import Mirror from "./components/Mirror";
@@ -6,57 +6,45 @@ import Pillar from "./components/Pillar";
 import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
 import Controller from "ecctrl";
 
-const keyboardMap = [
-  { name: "forward", keys: ["ArrowUp", "w", "W"] },
-  { name: "backward", keys: ["ArrowDown", "s", "S"] },
-  { name: "leftward", keys: ["ArrowLeft", "a", "A"] },
-  { name: "rightward", keys: ["ArrowRight", "d", "D"] },
-  { name: "jump", keys: ["Space"] },
-  { name: "run", keys: ["Shift"] },
-  { name: "crouch", keys: ["c", "C"] },
-];
-
 export function MirrorScene() {
   return (
     <>
       <Physics gravity={[0, -30, 0]} timeStep={"vary"}>
-        <KeyboardControls map={keyboardMap}>
-          {/** @ts-expect-error the export is slightly broken in TypeScript so just disabling the TS check here */}
-          <Controller
-            camInitDis={-0.01}
-            camMinDis={-0.01}
-            camFollowMult={100}
-            autoBalance={false}
-          >
-            <Sphere />
-          </Controller>
+        {/** @ts-expect-error the export is slightly broken in TypeScript so just disabling the TS check here */}
+        <Controller
+          camInitDis={-0.01}
+          camMinDis={-0.01}
+          camFollowMult={100}
+          autoBalance={false}
+        >
+          <Sphere />
+        </Controller>
 
-          <RigidBody type="fixed" colliders={false}>
-            <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
-              <planeGeometry args={[1000, 1000]} />
-              <meshStandardMaterial color="#2a2738" />
-            </mesh>
-            <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
-          </RigidBody>
+        <RigidBody type="fixed" colliders={false}>
+          <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+            <planeGeometry args={[1000, 1000]} />
+            <meshStandardMaterial color="#2a2738" />
+          </mesh>
+          <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+        </RigidBody>
 
-          <Mirror />
+        <Mirror />
 
-          <MemoryBlobs />
+        <MemoryBlobs />
 
-          <Pillar position={[-10, 6.9, 0]} />
-          <Pillar position={[-10, 6.9, 10]} />
-          <Pillar position={[-10, 6.9, 20]} />
-          <Pillar position={[10, 6.9, 0]} />
-          <Pillar position={[10, 6.9, 10]} />
-          <Pillar position={[10, 6.9, 20]} />
+        <Pillar position={[-10, 6.9, 0]} />
+        <Pillar position={[-10, 6.9, 10]} />
+        <Pillar position={[-10, 6.9, 20]} />
+        <Pillar position={[10, 6.9, 0]} />
+        <Pillar position={[10, 6.9, 10]} />
+        <Pillar position={[10, 6.9, 20]} />
 
-          <Brazier position={[10, 1.2, 5.5]} />
-          <Brazier position={[-10, 1.2, 5.5]} />
-          <Brazier position={[10, 1.2, 15.5]} />
-          <Brazier position={[-10, 1.2, 15.5]} />
-          <Brazier position={[10, 1.2, 25.5]} />
-          <Brazier position={[-10, 1.2, 25.5]} />
-        </KeyboardControls>
+        <Brazier position={[10, 1.2, 5.5]} />
+        <Brazier position={[-10, 1.2, 5.5]} />
+        <Brazier position={[10, 1.2, 15.5]} />
+        <Brazier position={[-10, 1.2, 15.5]} />
+        <Brazier position={[10, 1.2, 25.5]} />
+        <Brazier position={[-10, 1.2, 25.5]} />
       </Physics>
     </>
   );
