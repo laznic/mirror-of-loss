4b8dfb110b19f7aa988a4b2e3a274d787e502876
Niklas L <niklas@demtrees.com>
2023-12-16 21:40:21 +0200

    Add ecctrl to have better player movement control

--- src/App.tsx
+++ src/App.tsx
@@ -15,34 +15,24 @@ function App() {
 
   return (
     <>
-      <KeyboardControls
-        map={[
-          { name: "forward", keys: ["ArrowUp", "w", "W"] },
-          { name: "backward", keys: ["ArrowDown", "s", "S"] },
-          { name: "left", keys: ["ArrowLeft", "a", "A"] },
-          { name: "right", keys: ["ArrowRight", "d", "D"] },
-          { name: "jump", keys: ["Space"] },
-          { name: "crouch", keys: ["c", "C"] },
-        ]}
+      <Canvas
+        camera={{ fov: 90 }}
+        shadows
+        style={{ width: "100%", height: "100%" }}
+        onPointerDown={(e) => e.target.requestPointerLock()}
       >
-        <Canvas
-          camera={{ fov: 90 }}
-          shadows
-          style={{ width: "100%", height: "100%" }}
-        >
-          <ambientLight color={"#fff"} />
+        <ambientLight color={"#fff"} />
 
-          {/* <fog color="#000" attach="fog" near={50} far={700} /> */}
-          <directionalLight
-            intensity={5}
-            color={"#fff"}
-            position={[-3, 1.5, 3]}
-          />
+        {/* <fog color="#000" attach="fog" near={50} far={700} /> */}
+        <directionalLight
+          intensity={5}
+          color={"#fff"}
+          position={[-3, 1.5, 3]}
+        />
 
-          {/* <VoidScene /> */}
-          <MirrorScene />
-        </Canvas>
-      </KeyboardControls>
+        {/* <VoidScene /> */}
+        <MirrorScene />
+      </Canvas>
     </>
   );
 }
